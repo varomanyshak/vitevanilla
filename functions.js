7ab25515a2e1806e9cf75e17ef3d2eac8144cfdd
varomanyshak <varomanyshak@gmail.com>
2024-06-20 01:38:07 +0300

    fix cdn

--- functions.js
+++ functions.js
@@ -1,18 +1,19 @@
+import Swal from 'sweetalert2'
+import $ from 'jquery'
+
 const apiBaseUrl = 'https://aspnetclusters-175530-0.cloudclusters.net'
-let token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBdXRoZW50aWNhdGlvbiIsImp0aSI6IjExYjM2YmNiLTkxZDYtNDIzMC05ZTVjLTVjOTZiN2UwNzFmMyIsImlhdCI6MTcxODczNDYxMCwiaWQiOiI0IiwibmJmIjoxNzE4NzM0NjEwLCJleHAiOjE3MTg3MzgyMTAsImlzcyI6IkF1dGgiLCJhdWQiOiJBdXRoVXNlcnMifQ.efqPuq_Oli-wJOpqyyWDTX9SM2FjqOrn5V_pCbbOBlY'
 const showSignUpButton = document.getElementById('showSignUp');
 const showSignInButton = document.getElementById('showSignIn');
 const authContainer = document.getElementById('auth-container');
 const appContainer = document.getElementById('app');
 let accesses = [];
-import Swal from 'sweetalert2'
 
 export const showSignUp = () => {
-    authContainer.classList.add("right-panel-active");
+    authContainer.classList.add('right-panel-active');
 }
 
 export function showSignIn () {
-    authContainer.classList.remove("right-panel-active");
+    authContainer.classList.remove('right-panel-active');
 }
 
 export function trySignUp (event){
@@ -28,7 +29,6 @@ export function trySignUp (event){
         url: apiBaseUrl + '/User/addUser',
         data: JSON.stringify({username, password }),
         success: function (data) {
-            console.log('data', data)
             Swal.fire({
                 title: "Success",
                 text: "A new user has been registered successfully!",
@@ -60,8 +60,7 @@ export function trySignIn (event){
         data: JSON.stringify({ username, password }),
         url: apiBaseUrl + '/User/login',
         success: function (data) {
-            token = data.token
-            console.log('data', data)
+            let token = data.token
             token  = "Bearer " + token
             $.ajax({
                 type: "GET",
@@ -72,16 +71,17 @@ export function trySignIn (event){
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader('Authorization', token);
                 },
-                url: apiBaseUrl + '/User/getUserWithPages',
+                url: apiBaseUrl + '/User/getUsersAssignedPages',
                 data: { username: username },
-                success: function (data) {
+                success: function (dataR) {
                     appContainer.classList.add('logged-in')
-                    accesses = data.accesses
+                    accesses = dataR
                     let mItems = '';
-                    accesses.map(access=>{
-                        mItems += `<li><a>${access.pageName}</a></li>`
-                    })
-                    document.getElementById('menu-ul').innerHTML = mItems
+                    mItems += `<div class="navbar">`
+                    mItems += generateMenu(accesses);
+                    mItems += '<button onClick="logout()" style="position: absolute; right: 10px">Logout</button></div>'
+                    // document.getElementById('loginedUsername').innerHTML = data.username
+                    document.body.innerHTML = mItems + `<iframe style="width: 100vw; height: calc(100vh - 46px); margin-top: 46px" id="ift" src="/welcome.html"></iframe>`
                 }
             });
         },
@@ -98,4 +98,66 @@ export function trySignIn (event){
 
 export function logout () {
     appContainer.classList.remove('logged-in')
+    document.body.innerHTML = `<div id="app">
+      <div id="navmenu">
+      </div>
+      <div class="auth-container" id="auth-container">
+        <div class="form-container sign-up-container">
+          <form id="signUpForm" action="#" method="post">
+            <h1>Create Account</h1>
+            <input name="username" type="text" placeholder="Username" required />
+            <input name="password" type="password" placeholder="Password" required />
+            <button onclick="trySignUp(event)">Sign Up</button>
+          </form>
+        </div>
+        <div class="form-container sign-in-container">
+          <form id="signInForm" action="/" method="post">
+            <h1>Sign in</h1>
+            <input name="username" type="text" placeholder="Username" required />
+            <input name="password" type="password" placeholder="Password" required />
+            <button type="submit" id="signin_btn" onclick="trySignIn(event)">Sign In</button>
+          </form>
+        </div>
+        <div class="overlay-container">
+          <div class="overlay">
+            <div class="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>To keep connected with us please login with your personal info</p>
+              <button class="ghost" onclick="document.getElementById('auth-container').classList.remove('right-panel-active');">Sign In</button>
+            </div>
+            <div class="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <button class="ghost" onClick="document.getElementById('auth-container').classList.add('right-panel-active')">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main-container">
+      </div>
+    </div>`
 }
+export function loadHTML(url){
+    document.querySelector("iframe").src = `/${url.substring(1)}`;
+}
+const generateMenu = (items) => {
+    let menuHTML = '';
+    if(items.length > 0) {
+        items.map(item => {
+          if (item.parentId === null) {
+              const children = items.filter(child => child.parentId === item.pageId);
+              if (children.length > 0) {
+                  menuHTML += `<div class="dropdown"> <button class="dropbtn">${item.pageName}</button><div class="dropdown-content">`
+                  children.map(child => {
+                      menuHTML += `<button onClick="document.querySelector('iframe').src = '${child.internalUrl.substring(1)}'">${child.pageName}</button>`;
+                    });
+                    menuHTML += '</div></div>';
+            } else {
+                menuHTML += `<button onClick="document.querySelector('iframe').src = '${item.internalUrl.substring(1)}'">${item.pageName}</button>`;
+            }
+          }
+        });
+    }
+    return menuHTML;
+  }
+
